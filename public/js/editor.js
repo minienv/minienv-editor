0fcb670bca8cc962da99ee272f2921ba30c3c6fc
Mark Watson <mark.watson.x@gmail.com>
2017-07-05 00:25:42 -0500

    Upgraded to monaco 0.9

--- public/js/editor.js
+++ public/js/editor.js
@@ -7,13 +7,22 @@ require(['vs/editor/editor.main'], function() {
 });
 
 var getLanguageForExtension = function(ext) {
-	var language = "text";
-    if (ext == "css") {
+	var language = "plaintext";
+	if (ext == "cpp") {
+		language = "cpp";
+	}
+	else if (ext == "css") {
         language = "css";
     }
+	else if (ext == "go") {
+		language = "go";
+	}
     else if (ext == "htm" || ext == "html" || ext == "xhtml") {
         language = "html";
     }
+	else if (ext == "java") {
+		language = "java";
+	}
     else if (ext == "js") {
         language = "javascript";
     }
@@ -23,9 +32,27 @@ var getLanguageForExtension = function(ext) {
     else if (ext == "md") {
         language = "markdown";
     }
+	else if (ext == "php") {
+		language = "php";
+	}
+	else if (ext == "py") {
+		language = "python";
+	}
+	else if (ext == "rb") {
+		language = "ruby";
+	}
+	else if (ext == "sql") {
+		language = "sql";
+	}
     else if (ext == "swift") {
         language = "swift";
     }
+	else if (ext == "ts") {
+		language = "typescript";
+	}
+	else if (ext == "xml") {
+		language = "xml";
+	}
     else if (ext == "yml" || ext == "yaml") {
         language = "yaml";
     }
